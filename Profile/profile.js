dea26c33eb4efaa7982aeb30d48d798d6f5ed440
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-06-24 15:53:10 -0400

    update profile

---
+++ Profile/profile.js
@@ -0,0 +1,23 @@
+const loginData = getLoginData();
+function showProfile(user){
+    msgElement.value = user.bio;
+    bioElement.innerHTML = user.bio;
+}
+profileElement.addEventListener("click", () => {
+    fetch(apiBaseURL + "/api/users/" + loginData.username, {
+        method: "PUT",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            bio: msgElement.value
+
+        })
+    }).then(r => location = "/profile/profile.html")
+})
+fetch(apiBaseURL + "/api/users/" + loginData.username, {
+    headers: {
+        Authorization: `Bearer ${loginData.token}`,
+    },
+}).then(r => r.json()).then(showProfile)
